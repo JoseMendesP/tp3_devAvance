2bc4f1251a946a54bff95e8b37f47dd7b647cb85
Jose Mendes <josemendesper24@gmail.com>
2024-02-07 11:26:54 +0100

    la version qui ne marche pas

--- src/config/config.js
+++ src/config/config.js
@@ -36,3 +36,7 @@ export const getDebug = () => {
 export const getLocalBroker = () => {
     return process.env.HOST_IP !== ""
 }
+
+export const getConfigNumberWord = () => {
+    return Number(process.env.NUMBER_WORD) || 3
+}
--- src/server.js
+++ src/server.js
@@ -4,12 +4,14 @@ import * as Producer from "./redpanda/producer.js"
 import {getUser} from "./messages/userlist.js";
 import {getStringMessage} from "./messages/stringmessages.js";
 import {getNumberMessage} from "./messages/numbermessage.js";
-import {getConfigNumber, getDebug, getTimeOut, getTopic, getTypeMessage} from "./config/config.js";
+import {getConfigNumber, getDebug, getTimeOut, getTopic, getTypeMessage,getConfigNumberWord} from "./config/config.js";
 
 const configNumber = getConfigNumber()
 const typeMessage = getTypeMessage()
 const topic = getTopic()
 const debug = getDebug()
+const numberWord = getConfigNumberWord()
+
 
 async function start() {
 
